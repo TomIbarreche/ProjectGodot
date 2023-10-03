4eec3df6c79c8c78b5c6479ba62c743fa01056b1
Tom Ibarreche <ibarreche.tom@outlook.com>
2023-10-03 10:26:30 +0200

    Before implementing hurtBoxes

--- Scripts/pnj.gd
+++ Scripts/pnj.gd
@@ -1,9 +1,44 @@
 extends CharacterBody2D
+class_name PNJ
 
-var isMoving = false
+@export var speed = 50
+@export var limit = 0.5
+@export var endPoint: Marker2D
 
-@onready var animator = $AnimationPlayer 
+@onready var animator = $AnimatedSprite2D
 
+var startPosition
+var endPosition
+
+func _ready():
+	startPosition = position
+	endPosition = endPoint.global_position
+	
+func updateVelocity():
+	var moveDirection = endPosition - position
+	if moveDirection.length() < limit:
+		position = endPosition
+		changeDirection()
+	velocity = moveDirection.normalized() * speed
+	
+func changeDirection():
+	var tempEnd = endPosition
+	endPosition = startPosition
+	startPosition = tempEnd
+	
+func updateAnimation():
+	var animationString = "walkUp"
+	
+	if velocity.x < 0: 
+		animationString = "walkLeft"
+	elif velocity.x > 0: 
+		animationString = "walkRight"
+	elif velocity.y < 0: 
+		animationString = "walkUp"
+	
+	animator.play(animationString)
+	
 func _physics_process(delta):
-	if isMoving == false:
-		animator.play("idle")
+	updateVelocity()
+	move_and_slide()
+	updateAnimation()
